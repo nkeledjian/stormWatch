04be54bdbde7e606dc99c09b620368c8ebae7c73
Nareg Keledjian <NargMac@Naregs-MacBook-Pro-2.local>
2021-04-27 17:59:21 -0700

    experimenting with mui grid system

--- src/components/WeatherForecast/WeatherForecast.js
+++ src/components/WeatherForecast/WeatherForecast.js
@@ -3,27 +3,28 @@ import axios from 'axios';
 import WeatherCard from '../../containers/WeatherCard';
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
+// retreive API data
 export default function WeatherForecast() {
-    // retreive API data
-    const [ data, setData ] = useState('');
-  
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?id=5391959&units=imperial&appid=${API_KEY}`
-  
-    useEffect(() => {
-      fetchForecast();
-    }, []);
+  // set state variable 'data'
+  const [data, setData] = useState('');
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?id=5391959&units=imperial&appid=${API_KEY}`
 
-    const fetchForecast = () => {
-      axios.get(apiUrl)
+  useEffect(() => {
+    fetchForecast();
+  }, []);
+
+  const fetchForecast = () => {
+    axios.get(apiUrl)
       .then((res) => {
+        // point to api's forecast data (res.data.list) then update state variable with 'setData'
         const forecastData = res.data.list;
         setData(forecastData);
       })
       .catch(error => console.error(`Error: ${error}`));
-    };
+  };
 
-      return (
-        <WeatherCard data={data}/>
-      );
-    
+  return (
+    <WeatherCard data={data} />
+  );
+
 };
--- src/containers/WeatherCard/WeatherCard.js
+++ src/containers/WeatherCard/WeatherCard.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 275,
+  },
+  grid: {
+    flexGrow: 1
+  },
+  control: {
+    padding: theme.spacing(2),
   },
   title: {
     fontSize: 14,
@@ -14,15 +21,16 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 6,
   },
-});
+}));
 
 export default function WeatherCard(props) {
   const classes = useStyles();
   const displayWeatherData = props => {
     const {data} = props;
-    
+    // render if data is a available, else, render 'loading'
     if(data.length > 0) {
       return (
+        // work through array of objects containing forecast data
         data.map((item, index) => {
           console.log(item);
           return (
@@ -51,8 +59,12 @@ export default function WeatherCard(props) {
   }
   
   return (
-    <>
-      {displayWeatherData(props)}
-    </>
+    <Grid container className={classes.grid}>
+      <Grid item xs={12}>
+        <Grid container justify="center">
+          {displayWeatherData(props)}
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
