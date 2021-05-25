8045f69135a21cbe27c5d2edafb755b01e6ea98d
Nareg Keledjian <NargMac@Naregs-MacBook-Pro-2.local>
2021-05-25 16:30:04 -0700

    geolocation weather continued

--- src/components/GeolocationForecast/GeolocationForecast.js
+++ src/components/GeolocationForecast/GeolocationForecast.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import GeoForecast from '../../containers/GeoForecast';
+// import Geolocation from '../Geolocation';
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 // retreive API data, display forecast info
 export default function GeolocationForecast() {
-  // initialize state variable 'data'
+  // initialize state variables for weather data and geo coordinates
   const [data, setData] = useState('');
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
@@ -13,10 +14,24 @@ export default function GeolocationForecast() {
   const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=${API_KEY}`;
 
   useEffect(() => {
-    getLocation();
     fetchForecast();
   }, []);
 
+  const fetchForecast = () => {
+    axios.get(apiUrl)
+    .then(getLocation())
+      .then((res) => {
+        // forecast data response
+        const forecastData = res.data.daily;
+        // update state variable with response data
+        setData(forecastData);
+        // console.log('geoForecast', forecastData);
+      })
+      .catch(error => console.error(`Error: ${error}`));
+    
+  };
+
+  // next step: create separate component and import lat and lng data
   const getLocation = () => {
     // get geolocation
     if (!navigator.geolocation) {
@@ -33,18 +48,6 @@ export default function GeolocationForecast() {
       });
     }
   }
-
-  const fetchForecast = () => {
-    axios.get(apiUrl)
-      .then((res) => {
-        // forecast data response
-        const forecastData = res;
-        // update state variable with response data
-        setData(forecastData);
-        console.log('geoForecast', forecastData);
-      })
-      .catch(error => console.error(`Error: ${error}`));
-  };
 
   return (
     <div>
--- src/containers/GeoForecast/GeoForecast.js
+++ src/containers/GeoForecast/GeoForecast.js
@@ -7,10 +7,10 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    minWidth: 125,
   },
   grid: {
-    flexGrow: 1,
+    flexGrow: 0.5,
   },
   title: {
     fontSize: 14,
@@ -24,26 +24,35 @@ export default function GeoForecast(props) {
   const classes = useStyles();
   const displayGeoWeatherData = props => {
     const {data} = props;
+    
+    function timeConverter(UNIX_timestamp){
+      let a = new Date(UNIX_timestamp * 1000);
+      let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      let month = months[a.getMonth()];
+      let date = a.getDate();
+      let time = month + ' ' + date;
+      return time;
+    }
+    
     // render if data is a available, else, render 'loading'
-    if(data.length > 0) {
+    if(data) {
       return (
         // work through array of objects containing forecast data
         data.map((item, index) => {
-          console.log(item);
           return (
             <Card key={index} className={classes.root} variant="outlined">
               <CardContent>
                 <Typography variant="h5" component="h2">
-                  {Math.floor(item.main.temp)}°F
+                  {Math.floor(item.temp.day)}°F
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
                   {item.weather[0].description}
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
-                  Humidity: {item.main.humidity}%
+                  Humidity: {item.humidity}%
                 </Typography>
                 <Typography className={classes.title} color="textSecondary" gutterBottom>
-                  {item.dt_txt}
+                  {timeConverter(item.dt)}
                 </Typography>
               </CardContent>
             </Card>
